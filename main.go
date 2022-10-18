fef9bcb71e707552a3cad18941033815524836a4
punitarani <punitsai36@gmail.com>
2022-10-18 10:01:53 -0700

    Create a simple ListenAndServe server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = 8080
+
 func main() {
-	println("Simple API Server")
+	fmt.Println("Simple API Server")
+	fmt.Printf("Running on http://localhost:%d.\n", port)
+
+	// Start server on port 8080
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Printf("Failed to start server on http://localhost:%d.\n", port)
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
 }
