f1a3256a720d5551c0d76e05195b03f3fae3e090
punitarani <punitsai36@gmail.com>
2022-10-18 10:02:29 -0700

    Create a simple handler

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	fmt.Println("Simple API Server")
 	fmt.Printf("Running on http://localhost:%d.\n", port)
 
+	// Register handler function to "/" endpoint
+	http.HandleFunc("/", handler)
+
 	// Start server on port 8080
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
@@ -18,4 +21,14 @@ func main() {
 		fmt.Printf("Error: %s\n", err)
 		return
 	}
+}
+
+// Simple handler function
+func handler(w http.ResponseWriter, _ *http.Request) {
+	responseLength, err := w.Write([]byte("Simple API Server"))
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("Response status: %d. Response length: %d.\n", http.StatusOK, responseLength)
 }
